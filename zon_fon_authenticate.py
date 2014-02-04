5237f20e94d08f31960c5d555bbb56591968f452
João Pinto <lamego.pinto@gmail.com>
2014-02-04 20:48:56 +0000

    Added utf-8 encoding header

--- zon_fon_authenticate.py
+++ zon_fon_authenticate.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
+#
 #  Copyright 2014, João Pinto
 #  
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
 #  
 #  Unless required by applicable law or agreed to in writing, software
